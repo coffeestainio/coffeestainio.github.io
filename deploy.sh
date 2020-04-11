b9cc5e27736a3780fe15f552a5124f37f92f6d27
pablo <pjcalvov@gmail.com>
2020-04-10 22:02:11 -0600

    deploy again

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ cd dist
 git init
 git config --global user.email "pjcalvov@gmail.com"
 git config --global user.name "Pablo Calvo"
-git remote add origin git@github.com:coffeestainio/coffeestainio.github.io.git
+git remote set-url origin https://pjcalvo:"$ACCESS_TOKEN"@github.com/coffeestainio.github.io.git
 git add .
 git commit -m 'new deploy'
 git push --set-upstream origin master -f
