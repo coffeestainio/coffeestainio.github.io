a0e8fcf58b0addaff411f987e9bac855e9193973
Alex Gonzalez <alexgf08@gmail.com>
2020-04-11 10:53:13 -0600

    Changing button texts

--- src/views/components/OurServices.vue
+++ src/views/components/OurServices.vue
@@ -52,7 +52,7 @@
                                         <div class="card-body">
                                             <h5 class="card-title">Performance Testing</h5>
                                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                            <a href="#" class="btn btn-primary">Read More</a>
                                         </div>
                                     </div>
                                 </div>
@@ -61,7 +61,7 @@
                                         <div class="card-body">
                                             <h5 class="card-title">Testing Consultancy</h5>
                                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                            <a href="#" class="btn btn-primary">Read More</a>
                                         </div>
                                     </div>
                                 </div> 
