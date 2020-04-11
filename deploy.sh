331fbb61034a84fe2a2c36a18078af383612d37a
pablo <pjcalvov@gmail.com>
2020-04-10 21:14:23 -0600

    fixed github build

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 # run only on master
 cd dist
 git init
+git config --global user.email "pc@coffeestain.io"
+git config --global user.name "Pablo Calvo"
 git remote add origin git@github.com:coffeestainio/coffeestainio.github.io.git
 git add .
 git commit -m 'new deploy'
