cb2b8fb5589f73a8742c81c63c24103d583ae3b1
Alex Gonzalez <alexgf08@gmail.com>
2020-04-13 09:16:01 -0600

    Working on footer

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -1,16 +1,45 @@
 <template>
     <footer class="footer has-cards">
+            <div class="navbar-main" transparent type="" effect="light" expand>
+                <div class="container">
+                    <div class="row">
+                        <div slot="brand" class="col-12 col-lg-4">
+                            <img class="card-img-top cs-footer-logo" src="../../src/assets/img/clients/CofeeStein.png" alt="Coffestain logo">
+                        </div>
+                        <div slot="brand" class="col-12 col-lg-4"></div>
+                        <div slot="brand" class="col-12 col-lg-4">
+                            <ul class="navbar-nav align-items-lg-center ml-lg-auto cs-footer-items">
+                                <li class="nav-item">
+                                    <a class="nav-link nav-link-icon" href="https://twitter.com/pjcalvov" target="_blank"
+                                    data-toggle="tooltip" title="Follow us on Twitter">
+                                        <i class="fa fa-twitter-square"></i>
+                                        <span class="nav-link-inner--text d-lg-none">Twitter</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link nav-link-icon" href="https://www.linkedin.com/company/coffeestain-io/"
+                                    target="_blank" data-toggle="tooltip" title="Star us on Github">
+                                        <i class="fa fa-linkedin"></i>
+                                        <span class="nav-link-inner--text d-lg-none">LinkedIn</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item d-lg-block ml-lg-4">
+                                    <a href="mailto:info@coffeestain.io" target="_blank"
+                                    class="btn btn-neutral btn-icon">
+                                    <span class="btn-inner--icon">
+                                        <i class="fa fa-envelope mr-2"></i>
+                                    </span>
+                                        <span class="nav-link-inner--text">Contact Us</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </footer>
 </template>
-<script>
-export default {
-  name: 'app-footer',
-  data() {
-    return {
-      year: new Date().getFullYear()
-    }
-  }
-};
-</script>
 <style>
 </style>
+
+
