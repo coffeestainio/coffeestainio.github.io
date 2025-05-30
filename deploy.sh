5dff1168efd47411e8df89ae10c51acf80bed80a
Pablo Calvo <pjcalvov@gmail.com>
2025-05-30 13:08:28 +0000

    new release

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-# run only on master
-cd dist
-git init
-git config --global user.email "pjcalvov@gmail.com"
-git config --global user.name "Pablo Calvo"
-git remote add origin https://pjcalvo:"$ACCESS_TOKEN"@github.com/coffeestainio/coffeestainio.github.io.git
-git add .
-git commit -m 'new deploy'
-git push --set-upstream origin master -f
